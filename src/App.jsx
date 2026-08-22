567338ff8cff32a7f0be04e7e1c7a7ac73efcd4a
Rohit Nevare <rohitnevare02@gmail.com>
2026-08-22 23:51:36 +0530

    Final Barista Cafe project with deployment fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Home from "./Home";
 import About from "./About";
